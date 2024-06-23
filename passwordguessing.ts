ea143407414bbf54ace86c35846bdab1cf85ad2b
hars123 <harshitshukla4513@gmail.com>
2024-06-23 20:13:02 +0530

    add

--- passwordguessing.ts
+++ passwordguessing.ts
@@ -1,5 +1,5 @@
 let originaalnumber = 1231;
-let usernumber = prompt("Enter your number ");
+let usernumber  //prompt("Enter your number ");//
 while(usernumber !== originaalnumber){
  usernumber = prompt("try again");
 }
